59e430df4108d645337b73a957078a8909a086b8
Özgür Vatansever <ozgur.vatansever@markafoni.com>
2013-02-21 21:11:53 +0200

    version number and license added.

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -3,6 +3,8 @@ import atexit
 from async import process_pool
 from firebase import *
 
+__version__ = '0.1'
+VERSION = tuple(map(int, __version__.split('.')))
 
 @atexit.register
 def close_process_pool():
