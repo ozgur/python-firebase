854c4270a16ba3c483b8bcf3d7b2b436ec57c910
Mumin Ozturk <mumino@mumino.net>
2015-10-25 06:28:39 +0200

    register atexit callback and import multiprocessing module only when process_pool is initialized.
    1. Process pool is initialized by thread safe way.
    2. atexit callback only registered at the same time with process_pool initiliazing. So both of them are singleton in threaded env.
    3. "multiprocessing import" delayed as much as possible. this helps google app engine import issues.

--- firebase/async.py
+++ firebase/async.py
@@ -1,14 +1,37 @@
-import multiprocessing
-
+import threading
 from .lazy import LazyLoadProxy
 
 __all__ = ['process_pool']
 
 _process_pool = None
+_singleton_lock = threading.Lock()
+
+
 def get_process_pool(size=5):
     global _process_pool
-    if _process_pool is None:
-        _process_pool = multiprocessing.Pool(processes=size)
-    return _process_pool
-process_pool = LazyLoadProxy(get_process_pool)
+    global _singleton_lock
 
+    if _process_pool is not None:
+        return _process_pool
+
+    # initialize process_pool thread safe way
+    with _singleton_lock:
+        if _process_pool is None:
+            import atexit
+            import multiprocessing
+            _process_pool = multiprocessing.Pool(processes=size)
+
+            # atexit will work only if multiprocessing pool is initialized.
+            @atexit.register
+            def close_process_pool():
+                """
+                Clean up function that closes and terminates the process pool
+                """
+                _process_pool.close()
+                _process_pool.join()
+                _process_pool.terminate()
+
+    return _process_pool
+
+
+process_pool = LazyLoadProxy(get_process_pool)
